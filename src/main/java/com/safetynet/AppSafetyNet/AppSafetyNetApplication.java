3dcbf4091c3364dad9702ddd0854cfaa33ad6b03
Dylan Senasson <dylan.senasson@outlook.fr>
2025-06-02 02:55:01 +0200

    feat: initialisation du projet AppSafetyNet avec Spring Boot
    - Création du projet Spring Boot avec le starter web
    - Configuration de Log4j2 comme système de logging
    - Ajout des dépendances essentielles dans le pom.xml (Spring Boot Starter Web, Lombok, Log4j2, etc.)
    - Mise en place du fichier de configuration log4j2.xml dans src/main/resources
    - Structure initiale des packages et classe principale Application
    - Configuration Maven pour compiler, tester et packager le projet

---
+++ src/main/java/com/safetynet/AppSafetyNet/AppSafetyNetApplication.java
@@ -0,0 +1,13 @@
+package com.safetynet.AppSafetyNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSafetyNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSafetyNetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safetynet/AppSafetyNet/AppSafetyNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetynet.AppSafetyNet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSafetyNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
