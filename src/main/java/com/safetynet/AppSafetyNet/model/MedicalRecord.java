f73f71baaff931961936a348086c50df42f2661a
Dylan Senasson <dylan.senasson@outlook.fr>
2025-06-07 01:27:18 +0200

    feat(model): ajout de la méthode isMajor dans MedicalRecord
    - Implémentation de la méthode isMajor pour vérifier si la personne est majeure
    - Gestion du format de date "MM/dd/yyyy" avec DateTimeFormatter
    - Ajout d’une gestion des erreurs avec logs en cas de parsing invalide ou date manquante

--- src/main/java/com/safetynet/AppSafetyNet/model/MedicalRecord.java
+++ src/main/java/com/safetynet/AppSafetyNet/model/MedicalRecord.java
@@ -2,9 +2,15 @@ package com.safetynet.AppSafetyNet.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.List;
 
+@Slf4j
 @Data
 public class MedicalRecord implements UniqueEntity {
 
@@ -23,4 +29,19 @@ public class MedicalRecord implements UniqueEntity {
     @JsonProperty("allergies")
     private List<String> allergies;
 
+    public boolean isMajor() {
+        if (birthDate == null || birthDate.isEmpty()) {
+            log.warn("Birthdate is null or empty");
+            return false;
+        }
+
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+            LocalDate date = LocalDate.parse(birthDate, formatter);
+            return Period.between(date, LocalDate.now()).getYears() > 18;
+        } catch (DateTimeParseException e) {
+            log.error("Erreur lors de la transformation de la date de naissance : {}", e.getMessage());
+            return false;
+        }
+    }
 }
