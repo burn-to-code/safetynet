3ec4d334b64c78be03c4e77c101073ebbcb75e0f
Dylan Senasson <dylan.senasson@outlook.fr>
2025-06-10 20:23:54 +0200

    Refactor(childAlertDTO) : Délégation de la mise en forme des données par le childAlertDTO.

--- src/main/java/com/safetynet/AppSafetyNet/model/ChildAlertDTO.java
+++ src/main/java/com/safetynet/AppSafetyNet/model/ChildAlertDTO.java
@@ -1,18 +1,26 @@
 package com.safetynet.AppSafetyNet.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
 import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * DTO représentant les informations d'un enfant vivant à une adresse donnée
  * ainsi que la liste des autres personnes vivant sous le même toit.
  */
-@Data
-@AllArgsConstructor
-public class ChildAlertDTO {
-    private String firstName;
-    private String lastName;
-    private int age;
-    private List<String> personsInSameHouse;
+public record ChildAlertDTO(
+        String firstName,
+        String lastName,
+        int age,
+        List<String> personsInSameHouse
+){
+    public ChildAlertDTO(Person child, List<Person> personsAtAddress, MedicalRecord mr) {
+        this(child.getFirstName(),
+                child.getLastName(),
+                mr != null ? mr.getAge() : 0,
+                personsAtAddress.stream()
+                        .filter(p -> p.getLastName().equals(child.getLastName()) && !p.getFirstName().equals(child.getFirstName()))
+                        .map(UniqueEntity::getId)
+                        .collect(Collectors.toList())
+        );
+    }
 }
