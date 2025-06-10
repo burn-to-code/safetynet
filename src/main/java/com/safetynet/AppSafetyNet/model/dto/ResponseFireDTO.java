e747347f54384b9b46c49956090d95dd2777e3a7
Dylan Senasson <dylan.senasson@outlook.fr>
2025-06-10 20:45:59 +0200

    Feat(DTO) : Ajout de javaDoc dans ResponseFireDTO.java

--- src/main/java/com/safetynet/AppSafetyNet/model/dto/ResponseFireDTO.java
+++ src/main/java/com/safetynet/AppSafetyNet/model/dto/ResponseFireDTO.java
@@ -10,6 +10,14 @@ public record ResponseFireDTO(
         List<PersonsFireDTO> persons,
         String stationNumber
 ){
+    /**
+     * Constructeur permettant d'instancier nos différentes PersonFireDTO.
+     * Puis, de rajouter le numéro de la station correspondante à l'adresse (logique réalisé
+     * dans le service)
+     * @param persons une liste de person
+     * @param medicalRecords une liste de dossier medical
+     * @param fireStation une Station de pompier (doit être la bonne station, la méthode ne vérifie pas)
+     */
     public ResponseFireDTO(List<Person> persons, List<MedicalRecord> medicalRecords, FireStation fireStation){
         this(persons.stream()
                 .map(p -> {
@@ -25,6 +33,12 @@ public record ResponseFireDTO(
         );
     }
 
+    /**
+     * Une forme de réponse de personne pour la requête /fire
+     * @param persons un DTO utilisé précédemment, mais ayant la forme attendue pour notre requête
+     * @param medications la liste de medicament de la personne
+     * @param allergies la liste d'allergie de la personne
+     */
     public record PersonsFireDTO(
             PersonCoveredDTO.PersonInfoDTO persons,
             List<String> medications,
